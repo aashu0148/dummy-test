defc2e80698186f754175aeb92ba67a5ed72754b
Aashu Sharma <buildforfb@gmail.com>
2023-11-28 09:24:21 +0530

    small change

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get("/hi", (_req, res) => res.send("Hello there buddy!"));
 
 const getStockPastData = async (
   symbol,
-  from = Date.now() - 30 * 24 * 60 * 60 * 1000, // it is important to have 30-40 days data so that the algo can make good support and resistances
+  from = Date.now() - 20 * 24 * 60 * 60 * 1000, // it is important to have 20-30 days data so that the algo can make good support and resistances
   to,
   resolution = 5
 ) => {
